54f6388d38e523cf09dada9e0a633ca8c03e5691
dobshn <dobshn@gmail.com>
2024-10-31 22:56:46 +0900

    refactor: dosHeader 출럭 함수 생성

--- src/pe_parser.c
+++ src/pe_parser.c
@@ -206,6 +206,44 @@ void printDataDirectories(const IMAGE_DATA_DIRECTORY* dataDirectories, uint32_t 
     }
 }
 
+void printDosHeader(const IMAGE_DOS_HEADER *dosHeader) {
+    printf("*******************\n");
+    printf("* DOS Header의 정보 *\n");
+    printf("*******************\n");
+    printf("e_magic      : 0x%X\n", dosHeader->e_magic);
+    printf("e_cblp       : %u\n", dosHeader->e_cblp);
+    printf("e_cp         : %u\n", dosHeader->e_cp);
+    printf("e_crlc       : %u\n", dosHeader->e_crlc);
+    printf("e_cparhdr    : %u\n", dosHeader->e_cparhdr);
+    printf("e_minalloc   : %u\n", dosHeader->e_minalloc);
+    printf("e_maxalloc   : %u\n", dosHeader->e_maxalloc);
+    printf("e_ss         : 0x%X\n", dosHeader->e_ss);
+    printf("e_sp         : 0x%X\n", dosHeader->e_sp);
+    printf("e_csum       : 0x%X\n", dosHeader->e_csum);
+    printf("e_ip         : 0x%X\n", dosHeader->e_ip);
+    printf("e_cs         : 0x%X\n", dosHeader->e_cs);
+    printf("e_lfarlc     : 0x%X\n", dosHeader->e_lfarlc);
+    printf("e_ovno       : %u\n", dosHeader->e_ovno);
+    printf("e_res        : {0x%X, 0x%X, 0x%X, 0x%X}\n",
+           dosHeader->e_res[0], dosHeader->e_res[1],
+           dosHeader->e_res[2], dosHeader->e_res[3]);
+    printf("e_oemid      : 0x%X\n", dosHeader->e_oemid);
+    printf("e_oeminfo    : 0x%X\n", dosHeader->e_oeminfo);
+    printf("e_res2       : {0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X}\n",
+           dosHeader->e_res2[0], dosHeader->e_res2[1],
+           dosHeader->e_res2[2], dosHeader->e_res2[3],
+           dosHeader->e_res2[4], dosHeader->e_res2[5],
+           dosHeader->e_res2[6], dosHeader->e_res2[7],
+           dosHeader->e_res2[8], dosHeader->e_res2[9]);
+    printf("e_lfanew     : 0x%X\n", dosHeader->e_lfanew);
+
+    // DOS 스텁 정보 출력(크기)
+    printf("\n*******************\n");
+    printf("*  DOS Stub의 정보  *\n");
+    printf("*******************\n");
+    printf("DOS Stub의 크기: %lu 바이트\n", dosHeader->e_lfanew - sizeof(IMAGE_DOS_HEADER));
+}
+
 void printOptionalHeader(const IMAGE_OPTIONAL_HEADER32 *optionalHeader) {
     printf("***********************\n");
     printf("* Optional Header 정보 *\n");
@@ -249,16 +287,8 @@ void printNtHeader32(const IMAGE_NT_HEADERS32 *ntHeader) {
     printf("*******************\n");
     printf("Signature               : 0x%X\n", ntHeader->Signature);
 
-    // Signature가 올바른지 확인 (PE\0\0)
-    if (ntHeader->Signature != 0x00004550) {
-        printf("올바르지 않은 NT Header Signature입니다.\n");
-        return;
-    }
-
-    // File Header 출력
     printFileHeader(&ntHeader->FileHeader);
 
-    // Optional Header 출력
     printOptionalHeader(&ntHeader->OptionalHeader);
 }
 
@@ -271,53 +301,21 @@ int main() {
 
     IMAGE_DOS_HEADER dosHeader;
     fread(&dosHeader, sizeof(dosHeader), 1, peFile);
-
     if (dosHeader.e_magic != 0x5A4D) {
         printf("PE 파일이 아닙니다.\n");
         return 1;
     }
 
-    // DOS 헤더 정보 출력
-    printf("*******************\n");
-    printf("* DOS Header의 정보 *\n");
-    printf("*******************\n");
-    printf("e_magic      : 0x%X\n", dosHeader.e_magic);
-    printf("e_cblp       : %u\n", dosHeader.e_cblp);
-    printf("e_cp         : %u\n", dosHeader.e_cp);
-    printf("e_crlc       : %u\n", dosHeader.e_crlc);
-    printf("e_cparhdr    : %u\n", dosHeader.e_cparhdr);
-    printf("e_minalloc   : %u\n", dosHeader.e_minalloc);
-    printf("e_maxalloc   : %u\n", dosHeader.e_maxalloc);
-    printf("e_ss         : 0x%X\n", dosHeader.e_ss);
-    printf("e_sp         : 0x%X\n", dosHeader.e_sp);
-    printf("e_csum       : 0x%X\n", dosHeader.e_csum);
-    printf("e_ip         : 0x%X\n", dosHeader.e_ip);
-    printf("e_cs         : 0x%X\n", dosHeader.e_cs);
-    printf("e_lfarlc     : 0x%X\n", dosHeader.e_lfarlc);
-    printf("e_ovno       : %u\n", dosHeader.e_ovno);
-    printf("e_res        : {0x%X, 0x%X, 0x%X, 0x%X}\n",
-           dosHeader.e_res[0], dosHeader.e_res[1],
-           dosHeader.e_res[2], dosHeader.e_res[3]);
-    printf("e_oemid      : 0x%X\n", dosHeader.e_oemid);
-    printf("e_oeminfo    : 0x%X\n", dosHeader.e_oeminfo);
-    printf("e_res2       : {0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X, 0x%X}\n",
-           dosHeader.e_res2[0], dosHeader.e_res2[1],
-           dosHeader.e_res2[2], dosHeader.e_res2[3],
-           dosHeader.e_res2[4], dosHeader.e_res2[5],
-           dosHeader.e_res2[6], dosHeader.e_res2[7],
-           dosHeader.e_res2[8], dosHeader.e_res2[9]);
-    printf("e_lfanew     : 0x%X\n", dosHeader.e_lfanew);
-
-    // DOS 스텁 정보 출력(크기)
-    printf("\n*******************\n");
-    printf("*  DOS Stub의 정보  *\n");
-    printf("*******************\n");
-    printf("DOS Stub의 크기: %lu 바이트\n", dosHeader.e_lfanew - sizeof(IMAGE_DOS_HEADER));
-
-    // NT 헤더 정보 출력
     IMAGE_NT_HEADERS32 ntHeaders32;
     fseek(peFile, dosHeader.e_lfanew, SEEK_SET);
     fread(&ntHeaders32, sizeof(IMAGE_NT_HEADERS32), 1, peFile);
+    if (ntHeaders32.Signature != 0x00004550) {
+        printf("올바르지 않은 NT Header Signature입니다.\n");
+        return 1;
+    }
+
+    printDosHeader(&dosHeader);
+
     printNtHeader32(&ntHeaders32);
 
     return 0;
